4605dd75cccacd62ebb979c346770d9c4ef4b99f
GITAMVarunSai <vandra@gitam.in>
2025-03-15 16:56:03 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -1,18 +1,18 @@
-class p1 {
+class P1 {
     double x, y;
 
-    p1(double x, double y) {
+    P1(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    static double distance(Point p1, Point p2) {
+    static double distance(P1 p1, P1 p2) {
         return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
     }
 
     public static void main(String[] args) {
-        Point p1 = new Point(3, 4);
-        Point p2 = new Point(6, 8);
+        P1 p1 = new P1(3, 4);
+        P1 p2 = new P1(6, 8);
         System.out.println("Distance: " + distance(p1, p2));
     }
 }
